905fe020309da6cddc00df1b27345a750adb8ec1
Jastik98 <jastik414@gmail.com>
2024-10-08 18:06:47 +0200

    fix

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -4,16 +4,14 @@ import css from "./FriendList.module.css"
 const FriendListItem = ({avatar, name, isOnline}) => {
     return (
             <>
-            <ul className={css.friendsList}>
                 <li className={css.friendsListItem}>
             <img src={avatar} alt={name} width="48px" />
             <p className={css.friendsName}>{name}</p>
                     <p className={clsx(css.friendsStatus, {
                         [css.statusIsOnline]: isOnline === true,
-                        [css.statusIsOfline]: isOnline === false,
-                    })}>{isOnline === true ? "Online" : "Ofline"}</p>
+                        [css.statusIsOffline]: isOnline === false,
+                    })}>{isOnline === true ? "Online" : "Offline"}</p>
                     </li>
-                </ul>
         </>
     );
 }
